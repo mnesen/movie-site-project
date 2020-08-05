646b60a598dffad070918f03847ad95c6c8c4efc
Michael Nesen <mnesen@uwo.ca>
2020-08-05 02:07:46 -0400

    Uploading project to github

--- index.js
+++ index.js
@@ -118,40 +118,40 @@ const movieTemplate = (movieDetail) => {
     }, 0);
 
     const template = `
-    <article class="media">
-    <figure class="media-left">
-      <p class="image">
-        <img src="${movieDetail.Poster}" />
-      </p>
-    </figure>
-    <div class="media-content">
-      <div class="content">
-        <h1>${movieDetail.Title}</h1>
-        <h4>${movieDetail.Genre}</h4>
-        <p>${movieDetail.Plot}</p>
-      </div>
-    </div>
-    </article>
-    <article data-value=${awards} class="notification is-primary">
-        <p class="title">${movieDetail.Awards}</p>
-        <p class="subtitle">Awards</p>
-    </article>
-    <article data-value=${dollars} class="notification is-primary">
-        <p class="title">${movieDetail.BoxOffice}</p>
-        <p class="subtitle">Box Office</p>
-    </article>
-    <article data-value=${metascore} class="notification is-primary">
-        <p class="title">${movieDetail.Metascore}</p>
-        <p class="subtitle">Metascore</p>
-    </article>
-    <article data-value=${imdbRating} class="notification is-primary">
-        <p class="title">${movieDetail.imdbRating}</p>
-        <p class="subtitle">IMDB Rating</p>
-    </article>
-    <article data-value=${imdbVotes} class="notification is-primary">
-        <p class="title">${movieDetail.imdbVotes}</p>
-        <p class="subtitle">IMDB Votes</p>
-    </article>
+        <article class="media">
+        <figure class="media-left">
+        <p class="image">
+            <img src="${movieDetail.Poster}" />
+        </p>
+        </figure>
+        <div class="media-content">
+        <div class="content">
+            <h1>${movieDetail.Title}</h1>
+            <h4>${movieDetail.Genre}</h4>
+            <p>${movieDetail.Plot}</p>
+        </div>
+        </div>
+        </article>
+        <article data-value=${awards} class="notification is-primary">
+            <p class="title">${movieDetail.Awards}</p>
+            <p class="subtitle">Awards</p>
+        </article>
+        <article data-value=${dollars} class="notification is-primary">
+            <p class="title">${movieDetail.BoxOffice}</p>
+            <p class="subtitle">Box Office</p>
+        </article>
+        <article data-value=${metascore} class="notification is-primary">
+            <p class="title">${movieDetail.Metascore}</p>
+            <p class="subtitle">Metascore</p>
+        </article>
+        <article data-value=${imdbRating} class="notification is-primary">
+            <p class="title">${movieDetail.imdbRating}</p>
+            <p class="subtitle">IMDB Rating</p>
+        </article>
+        <article data-value=${imdbVotes} class="notification is-primary">
+            <p class="title">${movieDetail.imdbVotes}</p>
+            <p class="subtitle">IMDB Votes</p>
+        </article>
     `
 
     return template;
